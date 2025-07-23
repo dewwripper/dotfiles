3ee6fa649d031aa3eab06b4b62443179ebadc77f
Khanh CM <74553928+dewwripper@users.noreply.github.com>
2025-07-23 09:24:35 +0700

    Create .setup-aks.sh

---
+++ .setup-aks.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export XDG_CONFIG_HOME="$HOME"/.config
+mkdir -p "$XDG_CONFIG_HOME"
+
+# Create symlinks for existing configurations
+ln -sf "$PWD/.config/.bashrc" "$HOME"/.bashrc
+ln -sf "$PWD/.config/nvim" "$XDG_CONFIG_HOME"/nvim
+ln -sf "$PWD/.config/fish" "$XDG_CONFIG_HOME"/fish
+ln -sf "$PWD/.config/tmux" "$XDG_CONFIG_HOME"/tmux
+ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
+ln -sf "$PWD/.config/starship.toml" "$XDG_CONFIG_HOME"/starship.toml
+ln -sf "$PWD/.config/btop.conf" "$XDG_CONFIG_HOME"/btop.conf
+
+
+packages=(
+  starship
+  lazygit
+  fzf
+  fd
+  ripgrep
+  # make
+  # cmake
+  tmux
+  btop
+  eza
+  neovim
+  fish
+  gh
+
+  # Kubernetes and Azure Tools
+  azure-cli
+  kubectx
+  fluxcd/tap/flux
+  Azure/kubelogin/kubelogin
+)
+
+# Iterate over the array and install each package
+for package in "${packages[@]}"; do
+  echo "Installing $package..."
+  /home/linuxbrew/.linuxbrew/bin/brew install "$package"
+done
